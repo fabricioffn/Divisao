f37b12ccbd0922927a04ca5ef22f85d205f24c85
fabricio <fnmamao@gmail.com>
2020-06-15 11:34:24 -0300

    exericico

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace divisao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int  numerador;
+            int denominador;
+            Console.WriteLine("digite um numerador");
+            numerador = Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine("digite um denominador");
+            denominador = Convert.ToInt16(Console.ReadLine());
+
+            if(numerador > 0)
+            {
+                Console.WriteLine(numerador / denominador);
+            }
+            if(numerador == 0)
+            {
+                Console.WriteLine(numerador + " dividido por " + denominador + " é 0");
+            }
+            else if(denominador <= 0)
+            {
+                Console.WriteLine("não é possivel dividir por zero");
+            }
+
+
+
+
+
+
+
+
+        }
+    }
+}
